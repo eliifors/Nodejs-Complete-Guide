d1fb367b54f6cd36834b6e3e44213dec0071cb08
eliifors <elif.ors@ogr.sakarya.edu.tr>
2025-07-11 00:30:43 +0300

    Implement server and routing for user input handling

---
+++ 03-node-core-modules/Assigment1/app.js
@@ -0,0 +1,42 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Assigmnet1</title></head>");
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/users") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html><body>");
+    res.write("<ul>");
+    res.write("<li>User 1</li>");
+    res.write("<li>User 2</li>");
+    res.write("<li>User 3</li>");
+    res.write("</html></body>");
+    return res.end();
+  }
+
+  if (url === "/create-user") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody.split("=")[1]); // username=whatever-the-user-entered
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    res.end();
+  }
+});
+
+server.listen(3001);
