68ffabf9bfd3387e2c9feab3a07fa5719cb74acc
eliifors <145875619+eliifors@users.noreply.github.com>
2025-07-11 16:50:54 +0300

    Fix typo in title: Assigmnet -> Assignment

--- 03-node-core-modules/Assigment1/app.js
+++ 03-node-core-modules/Assigment1/app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
-    res.write("<head><title>Assigmnet1</title></head>");
+    res.write("<head><title>Assignment1</title></head>");
     res.write(
       '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
     );
