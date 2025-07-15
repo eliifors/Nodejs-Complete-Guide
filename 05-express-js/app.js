be01678ab309c802f9e6fc09c712b9321ff74a13
eliifors <145875619+eliifors@users.noreply.github.com>
2025-07-15 15:48:19 +0300

    Refactor app structure by adding routes for admin and shop, and include body-parser as a dependency

--- 05-express-js/app.js
+++ 05-express-js/app.js
@@ -1,22 +1,18 @@
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs!");
-  next();
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  res.send('<h1>The "Add Product" Page!</h1>');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from Express!<h1>");
+app.use("/admin", adminRoutes);
+app.use("/shop", shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
