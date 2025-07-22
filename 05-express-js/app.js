45255b83804d36afc7b7e51b0dc852869003067b
eliifors <145875619+eliifors@users.noreply.github.com>
2025-07-22 19:34:44 +0300

    Finished the Handlebars Template

--- 05-express-js/app.js
+++ 05-express-js/app.js
@@ -2,10 +2,19 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const { engine } = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine(
+  "hbs",
+  engine({
+    extname: "hbs",
+    defaultLayout: "main-layout",
+    layoutsDir: "views/layouts",
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
