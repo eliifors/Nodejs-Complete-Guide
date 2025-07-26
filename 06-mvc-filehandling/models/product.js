fe6ace3270b4580690b9a8e1648c093e054a27ff
eliifors <145875619+eliifors@users.noreply.github.com>
2025-07-27 00:25:07 +0300

    Refactor product model to use callback for file reading and streamline product fetching

--- 06-mvc-filehandling/models/product.js
+++ 06-mvc-filehandling/models/product.js
@@ -1,17 +1,29 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -19,17 +31,7 @@ module.exports = class Product {
     });
   }
 
-  static fetchAll() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        return [];
-      }
-      return JSON.parse(fileContent);
-    });
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
