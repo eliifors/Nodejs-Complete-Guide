75bb237bd3ee17961a15e134c5c79121ddd9a4b5
eliifors <145875619+eliifors@users.noreply.github.com>
2025-07-31 00:45:39 +0300

    add deleteById method

--- 06-mvc-filehandling/models/cart.js
+++ 06-mvc-filehandling/models/cart.js
@@ -39,20 +39,29 @@ module.exports = class Cart {
       if (err) {
         return;
       }
-      const updatedCart = { ...cart };
+      const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find((prod) => prod.id === id);
-      if (!product) {
-        return;
-      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
         (prod) => prod.id !== id
       );
       updatedCart.totalPrice =
         updatedCart.totalPrice - productPrice * productQty;
+
       fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
         console.log(err);
       });
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
     });
   }
 };
