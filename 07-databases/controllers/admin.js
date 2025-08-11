c4c62ae0e64f6fd70a2dbb7b994ac3b53f2f3ea0
eliifors <145875619+eliifors@users.noreply.github.com>
2025-08-11 22:32:28 +0300

    Refactor database connection and product model; update mongoConnect to set database instance, modify Product class to use MongoDB, and comment out unused routes and methods.

--- 07-databases/controllers/admin.js
+++ 07-databases/controllers/admin.js
@@ -18,56 +18,56 @@ exports.postAddProduct = (req, res, next) => {
   res.redirect("/");
 };
 
-exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
-  if (!editMode) {
-    return res.redirect("/");
-  }
-  const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    if (!product) {
-      return res.redirect("/");
-    }
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editing: editMode,
-      product: product,
-    });
-  });
-};
+// exports.getEditProduct = (req, res, next) => {
+//   const editMode = req.query.edit;
+//   if (!editMode) {
+//     return res.redirect("/");
+//   }
+//   const prodId = req.params.productId;
+//   Product.findById(prodId, (product) => {
+//     if (!product) {
+//       return res.redirect("/");
+//     }
+//     res.render("admin/edit-product", {
+//       pageTitle: "Edit Product",
+//       path: "/admin/edit-product",
+//       editing: editMode,
+//       product: product,
+//     });
+//   });
+// };
 
-exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  const updatedTitle = req.body.title;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedPrice = req.body.price;
-  const updatedDescription = req.body.description;
+// exports.postEditProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   const updatedTitle = req.body.title;
+//   const updatedImageUrl = req.body.imageUrl;
+//   const updatedPrice = req.body.price;
+//   const updatedDescription = req.body.description;
 
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedPrice,
-    updatedDescription
-  );
-  updatedProduct.save();
-  res.redirect("/admin/products");
-};
+//   const updatedProduct = new Product(
+//     prodId,
+//     updatedTitle,
+//     updatedImageUrl,
+//     updatedPrice,
+//     updatedDescription
+//   );
+//   updatedProduct.save();
+//   res.redirect("/admin/products");
+// };
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("admin/products", {
-      prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products",
-    });
-  });
-};
+// exports.getProducts = (req, res, next) => {
+//   Product.fetchAll((products) => {
+//     res.render("admin/products", {
+//       prods: products,
+//       pageTitle: "Admin Products",
+//       path: "/admin/products",
+//     });
+//   });
+// };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  // Delete the product from the database
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
-};
+// exports.postDeleteProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   // Delete the product from the database
+//   Product.deleteById(prodId);
+//   res.redirect("/admin/products");
+// };
