bcfede37c26bf539bf6d9814dcf1491c471c2d85
eliifors <145875619+eliifors@users.noreply.github.com>
2025-08-11 23:50:54 +0300

    modify

--- 07-databases/models/product.js
+++ 07-databases/models/product.js
@@ -1,19 +1,35 @@
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, imageUrl, price, description) {
     this.title = title;
+    this.imageUrl = imageUrl;
     this.price = price;
     this.description = description;
-    this.imageUrl = imageUrl;
   }
 
   save() {
     const db = getDb();
-    db.collection("products")
+    return db
+      .collection("products")
       .insertOne(this)
       .then((result) => {
         console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
       })
       .catch((err) => {
         console.log(err);
