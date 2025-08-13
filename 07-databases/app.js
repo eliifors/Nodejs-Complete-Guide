30ab543b41c421c2f2fd93cb3cce8079f41fd468
eliifors <145875619+eliifors@users.noreply.github.com>
2025-08-13 16:13:52 +0300

    Implement user authentication and cart functionality; update models, controllers, and views to integrate user data with cart operations.

--- 07-databases/app.js
+++ 07-databases/app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-
 const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -17,6 +17,15 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("689c73e77b8ca1ae02c6e4cc")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
