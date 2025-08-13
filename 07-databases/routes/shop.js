c1efd43451154b8d9ed8ba33bff288e26a21651f
eliifors <145875619+eliifors@users.noreply.github.com>
2025-08-13 23:49:54 +0300

    update order ID and product quantity references for consistency.

--- 07-databases/routes/shop.js
+++ 07-databases/routes/shop.js
@@ -20,6 +20,6 @@ router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
 router.get("/orders", shopController.getOrders);
 
-// router.get("/chechout", shopController.getCheckout);
+router.get("/chechout", shopController.getCheckout);
 
 module.exports = router;
